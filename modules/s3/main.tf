371248db2556915d1696269599613c6e745f4d1b
Subhransu26 <subhransusahu95@gmail.com>
2025-07-25 10:16:54 +0530

    fix: tfsec compliance for S3 bucket

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -9,3 +9,30 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default_encryption" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_logging" "logging" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+
+  target_bucket = aws_s3_bucket.artifact_bucket.id
+  target_prefix = "log/"
+}
+
